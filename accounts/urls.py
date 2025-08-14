ec82ddd4b2441c6fd1e5775d3a58546c57a6230e
axccun <jaiswarakki1310@gmail.com>
2025-08-14 12:51:50 +0530

    Django learning and projects

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('register',views.register,name='register'),
+    path('login_view',views.login_view,name='login_view'),
+    path('logout',views.logout,name='logout')
+]
